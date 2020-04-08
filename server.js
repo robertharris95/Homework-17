28f0d4146615a0fcdc8c07b32f6db17b7ae432aa
Robert Harris <rlh9@rice.edu>
2020-04-08 10:50:27 -0500

    server connected and primary schema created

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose")
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+//Connect Mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // Define API routes here
 
